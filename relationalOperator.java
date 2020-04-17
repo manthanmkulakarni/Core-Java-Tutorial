186cd0d955e21b992fccd192513634f87078c5ed
manthanmkulakarni <31768411+manthanmkulakarni@users.noreply.github.com>
2020-04-17 14:32:49 +0530

    Create relationalOperator.java

---
+++ relationalOperator.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author manthanmkulakarni
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        
+        int var1,var2;
+        
+        boolean result;
+        
+        var1=10;
+        
+        var2=3;
+        
+        //equal to operator
+        
+        result=var1==var2;
+        
+        System.out.println("eqal to "+result);
+        
+        //not equal 
+        
+        result=var1!=var2;
+        
+        System.out.println("not eqal to "+result);
+        
+        //greater than
+        
+        result=var1>var2;
+        System.out.println("greater than "+result);
+        
+        result=var1>=var2;
+        System.out.println("greater than or equal "+result);
+        
+        //less than
+        
+        result=var1<var2;
+        System.out.println("lesser than "+result);
+        
+        result=var1<=var2;
+        System.out.println("lesser than or equal "+result);     
+        
+
+
+        
+
+        
+        
+        
+        
+    }
+    
+}
